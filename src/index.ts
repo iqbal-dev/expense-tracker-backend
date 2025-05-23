ecb3a7568bcc49d44d7aa36baaed9636b669fcb3
iqbal-dev <pkmdiqbal@gmail.com>
2025-05-24 01:23:56 +0600

    setup logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import logger from './utils/logger';
+
 const numbers = 10;
 
 function main() {
+  logger.info('Application started');
+  logger.warn('This is a warning');
+
   return numbers;
 }
 
