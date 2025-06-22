7592e58002a43b488040bd878df9184ef51f7eb6
iqbal-dev <pkmdiqbal@gmail.com>
2025-06-23 01:07:18 +0600

    updated

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -1,8 +1,9 @@
+import authRoute from '@src/modules/auth/v1/auth.route';
 import userRoute from '@src/modules/user/v1/user.route';
 import { Router } from 'express';
 
 const router = Router();
 
 router.use('/users', userRoute);
-router.use('/auth', sdfgsdfgsdfsdfg);
+router.use('/auth', authRoute);
 export default router;
