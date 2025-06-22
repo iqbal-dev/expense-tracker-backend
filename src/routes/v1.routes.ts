a7d90592745c6eccfe5d03ba3fb91ba1ec98eedc
iqbal-dev <pkmdiqbal@gmail.com>
2025-06-23 01:03:45 +0600

    Test commit

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -4,5 +4,5 @@ import { Router } from 'express';
 const router = Router();
 
 router.use('/users', userRoute);
-router.use('/auth', sdfgsdfg);
+router.use('/auth', sdfgsdfgsdfsdfg);
 export default router;
